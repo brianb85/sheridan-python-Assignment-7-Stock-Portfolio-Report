1aa19e9a49e9718b4542b4ab82cc545d1090732d
Puja <getpuja11@gmail.com>
2024-12-16 11:26:34 -0500

    Assignment-7

---
+++ portfolio/portfolio_report.py
@@ -0,0 +1,53 @@
+"""
+Generates performance reports for your stock portfolio.
+"""
+import argparse
+import csv
+from collections import OrderedDict
+import requests
+
+
+def main():
+    """
+    Entrypoint into program.
+    """
+  
+
+
+def read_portfolio(filename):
+    """
+    Returns data from a CSV file
+    """
+   
+
+
+def get_args(args=None):
+    """
+    Parse and return command line argument values
+    """
+    
+
+
+def get_market_data(stocks_list):
+    """
+    Get the latest market data for the given stock symbols
+    """
+    
+
+
+def calculate_metrics(input_file, market_data):
+    """
+    Calculates the various metrics of each of the stocks
+    """
+    
+
+
+def save_portfolio(output_data, filename):
+    """
+    Saves data to a CSV file
+    """
+    
+
+
+if __name__ == '__main__':
+    main()
